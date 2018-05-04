ff97fbc7d31aa05fa6ef2689de5dbcfd91fa2507
张亚兵 <zhangyabing@ofo.com>
2018-05-04 16:03:57 +0800

    update rollup config file

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,16 @@
+import babel from 'rollup-plugin-babel';
+
 export default {
     input: 'src/index.js',
     output: {
         file: 'dist/bundle.js',
         format: 'iife',
+        sourcemap: true,
         name: 'draw',
-    }
+    },
+    plugins: [
+        babel({
+            exclude: 'node_modules/**'
+        })
+    ]
 }
